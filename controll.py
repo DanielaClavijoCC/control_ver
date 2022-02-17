9ba74a8bb94e7fa8ccbd5b86769ed2b6d1061f40
daniela <est.danielaclavijo@iedjgc.edu.co>
2022-02-17 09:15:59 -0500

    1 de abril

---
+++ controll.py
@@ -0,0 +1,4 @@
+print("mi primer programa 24 de enero")
+print("mi segunda linea de codigo 1 de febrero")
+print("mi tercer cambio 18 de febrero")
+print ("super cambio echo por daniela 1 de abril")
