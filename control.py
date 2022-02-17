b66526d125f030cfc9208d398331a5e70ba59b9d
daniela <est.danielaclavijo@iedjgc.edu.co>
2022-02-17 08:46:31 -0500

    18 de febrero

--- control.py
+++ control.py
@@ -1,3 +1,4 @@
 print("mi primer programa 24 de enero")
 print ("mi segunda linea de codigo 1 de febrero")
 print("mi tercer linea de cambio 18 de febrero")
+print("mi cuarta linea de cambio 30 de febrero")
